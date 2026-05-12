97d7b8c9bf8aa446f79ed460b96a05adf0f50bc9
kalhara05 <kalhara139@gmail.com>
2026-05-12 14:59:27 +0530

    Add name and mobile fields to User model
    Introduce private name and mobile properties to lk.jiat.bcd.model.User with a no-arg constructor and standard getters/setters. This enables storing basic user contact info and supports frameworks that require JavaBean-style accessors (e.g., serialization or data binding).

--- src/main/java/lk/jiat/bcd/model/User.java
+++ src/main/java/lk/jiat/bcd/model/User.java
@@ -1,4 +1,26 @@
 package lk.jiat.bcd.model;
 
 public class User {
+
+    private String name;
+    private String mobile;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 }
